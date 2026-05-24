aace6d4b8213c211295049d21cff854b3647eec7
samm-e13 <navigatorddg54.com>
2026-05-24 00:13:36 -0400

    js added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const foot = document.createElement("footer");
+document.body.appendChild(foot);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+const copyright = document.createElement("p");
+copyright.innerHTML = "Ortiz, " + thisYear;
+foot.appendChild(copyright);
+
+const skills = [
+  "Snowflake/Vertica/SQL",
+  "Tableau",
+  "R Software",
+  "Statistical Analysis",
+  "Basic Modeling",
+];
+
+const skillsSection = document.querySelector("#skills");
+const skillsList = skillsSection.querySelector("ul");
+
+for (let i = 0; i < skills.length; i++) {
+  let skill = document.createElement("li");
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
+}
