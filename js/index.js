0660c20c5861989994f4308c61c874a73daf6992
samm-e13 <navigatorddg54.com>
2026-06-01 23:27:43 -0600

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -23,3 +23,26 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+
+const messageForm = document.querySelector("#leave_message");
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const usersName = event.target.usersName;
+  const usersEmail = event.target.usersEmail;
+  const usersMessage = event.target.usersMessage;
+  console.log(usersName, usersEmail, usersMessage);
+  const messageSection = document.querySelector("#messages");
+  const messageList = messages.querySelector("ul");
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>: <span>${usersMessage}</span>`;
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+  removeButton.addEventListener("click", function (event) {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+  event.target.reset();
+});
